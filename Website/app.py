25db668f75b181a2f7a8e7a16cd350df20116f0f
Stephanie Contino <olivetalia@gmail.com>
2023-04-18 16:30:04 -0400

    Began frontend

--- Website/app.py
+++ Website/app.py
@@ -45,8 +45,8 @@ def form():
 
 
 # handle venue POST and serve result web page
-@app.route('/venue-handler', methods=['POST'])
-def venue_handler():
+@app.route('/map-handler', methods=['POST'])
+def map_handler():
     rows = connect('SELECT venue_id, title FROM events WHERE venue_id = ' + request.form['venue_id'] + ';')
     heads = ['venue_id', 'title']
     return render_template('my-result.html', rows=rows, heads=heads)
