d881c288e0bc39f8f06379bd60ff7e2b5ad957ba
Stephanie Contino <olivetalia@gmail.com>
2023-04-21 14:04:51 -0400

    Update app

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def emission_list():
     county_name = request.form['county_name']
     rows = connect('SELECT mname, county, year FROM municipality WHERE county = ' + request.form['county_name'] + ';')
     heads = ['mname', 'county', 'year']
-    return render_template('emission-list.html', rows=rows, heads=heads, county=county_name)
+    return render_template('emission-list.html', rows=rows, heads=heads, county_name=county_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
