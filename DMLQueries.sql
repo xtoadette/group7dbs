38278a31345c51a31569e6bc1362490f73c6cbba
Stephanie Contino <contins1@tcnj.edu>
2023-04-22 21:24:17 -0400

    Update DML

--- DMLQueries.sql
+++ DMLQueries.sql
@@ -18,31 +18,23 @@ SELECT SUM(combination_long_haul_truck), SUM (combination_short_haul_truck),
     WHERE  TE.Year IN (2015, 2017, 2019, 2020) AND TE.Total = (SELECT MAX(Total) FROM Total_emissions WHERE Mname = TE.Mname AND County = TE.County AND Year = TE.Year
     );
 
---How many EV vehicles does each municipality have?
-    SELECT municipality.Name, SUM(vehicles.# of EV’s) AS num_ev_vehicles
-    FROM municipality
-    JOIN vehicles
-    ON municipality.Name = vehicles.Name AND municipality.County = vehicles.County AND municipality.Year = vehicles.Year
-    WHERE vehicles.Vehicles = '# of EV’s’
-    GROUP BY municipality.Name;
+--How many EV vehicles does each county have?
+    SELECT county, SUM(EVs) AS num_evs
+    FROM vehicles
+	WHERE year = 2020
+	GROUP BY county
+	ORDER BY num_evs DESC;
 
---How many gas cars does each municipality have?
-    SELECT municipality.Name, SUM(vehicles.Gas Cars) as Gas_Cars
-    FROM municipality
-    JOIN vehicles
-    ON municipality.Name = vehicles.Name
-    AND municipality.County = vehicles.County
-    AND municipality.Year = vehicles.Year
-    GROUP BY municipality.Name;
+--How many gas cars does each county have?
+    SELECT county, SUM(Gas) AS num_gas
+    FROM vehicles
+	WHERE year = 2020
+	GROUP BY county
+	ORDER BY num_gas DESC;
 
 --What are the total number of vehicles does each municipality have in years 2015 and 2020?
-    SELECT Name, County, Year, SUM(Total Personal Vehicles) as Total_Vehicles
+    SELECT Name, County, Year, vehicles
     FROM vehicles
     WHERE Year IN (2015, 2020)
-    GROUP BY Name, County;
-
---Retrieve the total amount of emissions for passenger cars in each municipality
-    SELECT municipality.Name, municipality.County, municipality.Year, SUM(Passenger Cars Total Emission) AS Total_Passenger_Car_Emissions
-    FROM municipality
-    JOIN Total_emissions ON municipality.Name = Total_emissions.Mname AND municipality.County = Total_emissions.County AND municipality.Year = Total_emissions.Year
-    GROUP BY municipality.Name, municipality.County, municipality.Year;
+	ORDER BY year;
+   
